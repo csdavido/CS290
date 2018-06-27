35446fe5fccd069f439d3f6cbd04ce968e399382
David Rider <riderda@oregonstate.edu>
2018-06-27 23:25:38 +0900

    Add files via upload

---
+++ file1.cpp
@@ -0,0 +1,10 @@
+//file1.cpp
+//A basic C++ file
+#include <iostream>
+
+int main() {
+
+	std::cout << "This is a file." << std::endl;
+
+return 0;
+}
---
+++ file2.cpp
@@ -0,0 +1,10 @@
+//file2.cpp
+//A basic C++ file
+#include <iostream>
+
+int main() {
+
+	std::cout << "This too is also a file" << std::endl;
+
+return 0;
+}
